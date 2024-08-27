733a16adb2e86b4993d5a6420b8489d9837352c5
Jayashre <jaya2004kra@gmail.com>
2024-08-27 21:11:27 +0530

    modified

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { scanDependencies } from './dependencyScanner';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.checkDependencies', async () => {
+        const folders = vscode.workspace.workspaceFolders;
+        if (folders) {
+            const folder = folders[0].uri.fsPath;
+            try {
+                await scanDependencies(folder);
+                vscode.window.showInformationMessage('Dependency scan complete.');
+            } catch (error: unknown) {
+                if (error instanceof Error) {
+                    vscode.window.showErrorMessage(`Error scanning dependencies: ${error.message}`);
+                } else {
+                    vscode.window.showErrorMessage('An unknown error occurred.');
+                }
+            }
+        } else {
+            vscode.window.showInformationMessage('No folder open.');
+        }
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "secuscan" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('secuscan.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SecuScan!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
