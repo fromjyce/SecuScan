d9af9a458916ff270592f998693237de8929deee
Jayashre <jaya2004kra@gmail.com>
2024-08-29 20:32:59 +0530

    modidfied

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -1,16 +1,12 @@
 import * as path from 'path';
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 async function main() {
     try {
-        await runTests({
-            extensionDevelopmentPath: path.resolve(__dirname, '../../'),
-            extensionTestsPath: path.resolve(__dirname, './extension.test.ts'),
-            launchArgs: [
-                path.resolve(__dirname, 'testFixture'),
-                '--disable-extensions'
-            ],
-        });
+        const extensionDevelopmentPath = path.resolve(__dirname, '../../');
+        const extensionTestsPath = path.resolve(__dirname, './extension.test.js');
+
+        await runTests({ extensionDevelopmentPath, extensionTestsPath });
     } catch (err) {
         console.error('Failed to run tests');
         process.exit(1);
